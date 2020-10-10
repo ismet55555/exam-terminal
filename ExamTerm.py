cdf5562033348997d9454589f0ef5a39de0e70e6
ismet55555 <ismet.handzic@gmail.com>
2020-10-10 12:58:35 -0400

    color and decor, formatting menu

--- ExamTerm.py
+++ ExamTerm.py
@@ -23,6 +23,10 @@ class Exam:
         self.exam_contents = self.__load_parse_examfile(exam_filepath)
 
         self.color = {}
+        self.decor = {}
+
+        self.height_limit = 20
+        self.width_limit = 80
 
         self.questions_total = len(self.exam_contents['questions'])
         self.questions_complete = 0
@@ -116,7 +120,7 @@ class Exam:
         curses.curs_set(0)
 
         # Load curses colors
-        self.color = self.__load_curses_colors()
+        self.color, self.decor = self.__load_curses_colors_decor()
 
         # Turn off echo
         curses.noecho()
@@ -124,41 +128,37 @@ class Exam:
         # Non-blocking for user
         scr.nodelay(True)
 
-    def __draw_screen_border(self, scr, color_pair_index) -> None:
-        scr.attron(curses.color_pair(color_pair_index))
+    def __draw_screen_border(self, scr, color) -> None:
+        scr.attron(color)
         scr.border(0)
-        scr.attroff(curses.color_pair(color_pair_index))
+        scr.attroff(color)
 
-    def __draw_horizontal_sceen_seperator(self, scr, y, color_pair_index) -> None:
+    def __draw_horizontal_sceen_seperator(self, scr, y, color) -> None:
         # Getting the screen height and width
         term_height, term_width = scr.getmaxyx()
 
-        scr.attron(curses.color_pair(color_pair_index))
-
-        if y < term_height - 2 and y > 2:
+        if y < term_height - 2 and y > 1:
             for x in range(term_width - 2):
-                scr.addstr(y, x + 1, '_', curses.color_pair(6) | curses.A_BOLD)
+                scr.addstr(y, x + 1, '-', color)
 
-        scr.attroff(curses.color_pair(color_pair_index))
-
-    def __check_terminal_size(self, scr, height_limit, width_limit) -> bool:
+    def __check_terminal_size(self, scr) -> bool:
         terminal_size_good = True
 
         # Getting the screen height and width
         term_height, term_width = scr.getmaxyx()
         
         # Check Height
-        if term_height < height_limit:
-            scr.addstr(1, 1, f"Terminal height must be more than {height_limit} characters!", curses.color_pair(1))
+        if term_height < self.height_limit:
+            scr.addstr(1, 1, f"Terminal height must be more than {self.height_limit} characters!", curses.color_pair(1))
             terminal_size_good = False
 
         # Check Width
-        if term_width < width_limit:
-            scr.addstr(2, 1, f"Terminal width must be more than {width_limit} characters!", curses.color_pair(1))
+        if term_width < self.width_limit:
+            scr.addstr(2, 1, f"Terminal width must be more than {self.width_limit} characters!", curses.color_pair(1))
             terminal_size_good = False
 
         if terminal_size_good:
-            scr.addstr(0, 0, f"[Terminal Size: W:{term_width}, H:{term_height}]", curses.color_pair(1))
+            scr.addstr(1, 1, f"[Terminal Size: W:{term_width}, H:{term_height}]", curses.color_pair(1))
 
         return terminal_size_good
 
@@ -203,44 +203,68 @@ class Exam:
     ###############################################################################################
 
     @staticmethod
-    def __load_curses_colors() -> dict:
+    def __load_curses_colors_decor() -> tuple:
+        # In code usage example:
+        #       scr.addstr(y, x, "hello", self.color['blue'])
+        #       scr.addstr(y, x, "hello", self.color['blue'] | self.decor['bold'])
+
         # Start colors in curses
         try: curses.start_color()
         except: pass
 
-        # Define the colors to be used (foreground and background) (ie. curses.color_pair(1))
-        #     curses.init_pair(color reference index, font color number, background color number)
-
-        # Define colors
+        # Defining colors [foreground/font, background]
         color_definition = {
-            'default':  [curses.COLOR_WHITE, 0],
-            'cyan':     [curses.COLOR_CYAN, 0],
-            'red':      [curses.COLOR_RED, 0],
-            'magenta':  [curses.COLOR_MAGENTA, 0],
-            'green':    [curses.COLOR_GREEN, 0],
-            'yellow':   [curses.COLOR_YELLOW, 0],
-            'gray':     [240, 0]
+            'default':      [curses.COLOR_WHITE, 0],
+            'red':          [curses.COLOR_RED, 0],
+            'green':        [curses.COLOR_GREEN, 0],
+            'blue':         [curses.COLOR_BLUE, 0],
+            'yellow':       [curses.COLOR_YELLOW, 0],
+            'cyan':         [curses.COLOR_CYAN, 0],
+            'magenta':      [curses.COLOR_MAGENTA, 0],
+            'grey':         [240, 0],
+            'black-white':  [curses.COLOR_BLACK, curses.COLOR_WHITE],
+            'white-red':    [curses.COLOR_WHITE, curses.COLOR_RED]
         }
 
         # Initiating curses color and saving for quick reference
         color = {}
         for index, (key, value) in enumerate(color_definition.items()):
             curses.init_pair(index + 1, value[0], value[1])
-            color[key] = index + 1
+            color[key] = curses.color_pair(index + 1)
 
-        return color
+        decoration_definition ={
+            'normal' : curses.A_NORMAL,         # Normal display (no highlight)
+            'standout': curses.A_STANDOUT,      # Best highlighting mode of the terminal
+            'underline': curses.A_UNDERLINE,    # Underlining
+            'reverse': curses.A_REVERSE,        # Reverse video
+            'blink': curses.A_BLINK,            # Blinking
+            'dim': curses.A_DIM,                # Half bright
+            'bold': curses.A_BOLD,              # Extra bright or bold
+            'protect': curses.A_PROTECT,        # Protected mode
+            'invisible': curses.A_INVIS,        # Invisible or blank mode
+            'alt-char': curses.A_ALTCHARSET,    # Alternate character set
+            'char': curses.A_CHARTEXT           # Bit-mask to extract a character
+        }
+
+        # Initiating curses color and saving for quick reference
+        decor = {}
+        for key, value in decoration_definition.items():
+            decor[key] = value
+
+        return color, decor
 
     @staticmethod
     def __load_keys():
         KEYS = {
             "ENTER": (curses.KEY_ENTER, ord('\n'), ord('\r')),
-            "KEYS_UP": (curses.KEY_UP, ord('k')),
-            "KEYS_DOWN": (curses.KEY_DOWN, ord('j')),
-            "KEYS_RIGHT": (curses.KEY_RIGHT, ord('l')),
-            "KEYS_LEFT": (curses.KEY_LEFT, ord('h')),
-            "KEYS_PAUSE": (ord('p'), ord('P')),
-            "KEYS_RESUME": (ord('r'), ord('R')),
-            "KEYS_QUIT": (27 , ord('q'), ord('Q'))
+            "SPACE": (32, ord(' ')),
+            "UP": (curses.KEY_UP, ord('k')),
+            "DOWN": (curses.KEY_DOWN, ord('j')),
+            "RIGHT": (curses.KEY_RIGHT, ord('l')),
+            "LEFT": (curses.KEY_LEFT, ord('h')),
+            "PAUSE": (ord('p'), ord('P')),
+            "RESUME": (ord('r'), ord('R')),
+            "QUIT": (27 , ord('q'), ord('Q'))
         }
         return KEYS
 
@@ -253,7 +277,14 @@ class Exam:
             "  |_||___|___|_| |_|_|_|_  |    |_||___|_| |_|_|_|_|_|_|__,|_|",
             "                       |___|                                  ",
         ]
+        software_name = [
+            "Testing Terminal v0.1"
+        ]
         return software_name
+
+    @staticmethod
+    def __center_x(display_width, line) -> int:
+        return display_width // 2 - len(line) // 2
 
     ###############################################################################################
 
@@ -273,32 +304,32 @@ class Exam:
             ########################################################################################
 
             # Check user keyboard input
-            if k in KEYS['KEYS_DOWN']:
+            if k in KEYS['DOWN']:
                 self.selection_index += 1
 
-            elif k in KEYS['KEYS_UP']:
+            elif k in KEYS['UP']:
                 self.selection_index -= 1
 
             elif k in KEYS['ENTER']:
                  return
 
-            elif k in KEYS['KEYS_QUIT']:
+            elif k in KEYS['QUIT']:
                 break
 
             ########################################################################################
 
-            term_height, term_width = scr.getmaxyx()              
+            term_height, term_width = scr.getmaxyx()          
 
             ########################################################################################
 
             # Check terminal size
-            terminal_size_good = self.__check_terminal_size(scr, height_limit=30, width_limit=80)
+            terminal_size_good = self.__check_terminal_size(scr)
             if not terminal_size_good:
                 # TODO: Do something here ...
                 pass
 
             # Drawing the screen border
-            self.__draw_screen_border(scr, color_pair_index=6)
+            self.__draw_screen_border(scr, self.color['grey'])
 
             ########################################################################################
 
@@ -310,58 +341,59 @@ class Exam:
             software_name = self.__load_software_ascii_name()
             start_x = term_width // 2 - len(software_name[0]) // 2
             for y, line in enumerate(software_name):
-                scr.addstr(2 + y, start_x, line, curses.color_pair(6) | curses.A_BOLD)
+                scr.addstr(1 + y, start_x, line, self.color['grey'])
 
             # Horizontal Seperator
-            self.__draw_horizontal_sceen_seperator(scr, y + 3, self.color['green'])
+            self.__draw_horizontal_sceen_seperator(scr, y + 2, self.color['grey'])
 
             ########################################################################################
 
             # TODO: text wrapper
 
-            y = 10           
+            y = 4
             start_x = [10, 26]
 
-            lines = ["Exam Title:", f"{self.exam_contents['exam']['exam_title']}"]
-            for x, line in zip(start_x, lines):
-                scr.addstr(y + 0, x, line, curses.color_pair(1) | curses.A_BOLD)
+            line = f"{self.exam_contents['exam']['exam_title']}"
+            # lines = ["Exam Title:", f"{self.exam_contents['exam']['exam_title']}"]
+            # for x, line in zip(start_x, lines):
+            scr.addstr(y + 0, self.__center_x(term_width, line), line, self.decor['bold'])
 
-            lines = ["Composed By:", f"{self.exam_contents['exam']['exam_author']}"]
-            for x, line in zip(start_x, lines):
-                scr.addstr(y + 1, x, line, curses.color_pair(1))
+            line = f"{self.exam_contents['exam']['exam_author']}"
+            # lines = ["Composed By:", f"{self.exam_contents['exam']['exam_author']}"]
+            # for x, line in zip(start_x, lines):
+            scr.addstr(y + 1, self.__center_x(term_width, line), line, self.color['grey'])
 
-            lines = ["Edit Date:", f"{self.exam_contents['exam']['exam_edit_date']}"]
-            for x, line in zip(start_x, lines):
-                scr.addstr(y + 2, x, line, curses.color_pair(1))
+            line = f"{self.exam_contents['exam']['exam_edit_date']}"
+            # lines = ["Edit Date:", f"{self.exam_contents['exam']['exam_edit_date']}"]
+            # for x, line in zip(start_x, lines):
+            scr.addstr(y + 2, self.__center_x(term_width, line), line, self.color['grey'])
 
 
+            y += 0
 
             lines = ["Description:", f"{self.exam_contents['exam']['exam_description']}"]
             for x, line in zip(start_x, lines):
-                scr.addstr(y + 4, x, line, curses.color_pair(1))
+                scr.addstr(y + 4, x, line, self.color['default'])
             
-            lines = ["Exam Type:", f"Multiple Choice, Single Choice"]
+            lines = ["Exam Type:", f"Multiple Choice, Single Answer"]
             for x, line in zip(start_x, lines):
-                scr.addstr(y + 5, x, line, curses.color_pair(1))
+                scr.addstr(y + 5, x, line, self.color['default'])
 
             lines = ["Questions:", f"52"]
             for x, line in zip(start_x, lines):
-                scr.addstr(y + 6, x, line, curses.color_pair(1))
+                scr.addstr(y + 6, x, line, self.color['default'])
 
             lines = ["Allowed Time:", f"{self.exam_contents['exam']['exam_allowed_time']} {self.exam_contents['exam']['exam_allowed_time_units']}"]
             for x, line in zip(start_x, lines):
-                scr.addstr(y + 7, x, line, curses.color_pair(1))
+                scr.addstr(y + 7, x, line, self.color['default'])
 
             lines = ["Passing Score:", f"{self.exam_contents['exam']['exam_passing_score']} %"]
             for x, line in zip(start_x, lines):
-                scr.addstr(y + 8, x, line, curses.color_pair(1))
+                scr.addstr(y + 8, x, line, self.color['default'])
 
             ########################################################################################
 
-            # Horizontal Seperator
-            self.__draw_horizontal_sceen_seperator(scr, y + 12, curses.color_pair(6))
-
-            ########################################################################################
+            y_selection = term_height - 9
 
             selections = [
                 "Begin Exam",
@@ -372,13 +404,25 @@ class Exam:
             self.selection_index = max(self.selection_index, 0)
             self.selection_index = min(self.selection_index, len(selections) - 1)
 
+            # Get the x position of selection indicator
+            longest_selection_text = len(max(selections, key = len))
+            x_begin = term_width // 2 - longest_selection_text // 2 - 3
+            x_end = term_width // 2 + longest_selection_text // 2 + 2
+
             for s, selection in enumerate(selections):
                 if s == self.selection_index:
                     # Color for highlighted selection text
-                    color = curses.A_BOLD 
+                    color = self.color['default'] | self.decor['bold']
+                    scr.addstr(y + s + 1 + y_selection, x_begin, '|', color)
+                    scr.addstr(y + s + 1 + y_selection, x_end, '|', color)
                 else:
-                    color = curses.color_pair(6)
-                scr.addstr(y + s + 14, term_width // 2 - len(selection) // 2, selection, color)
+                    color = self.color['grey']
+                scr.addstr(y + s + 1 + y_selection, term_width // 2 - len(selection) // 2, selection, color)
+
+            ########################################################################################
+
+            # Horizontal Seperator
+            self.__draw_horizontal_sceen_seperator(scr, term_height - 6, self.color['grey'])
 
             ########################################################################################
 
@@ -387,7 +431,6 @@ class Exam:
 
             # Get User input
             k = scr.getch()
-
 
         return
 
@@ -432,11 +475,11 @@ class Exam:
             ########################################################################################
 
             # Check user keyboard input
-            if k in KEYS['KEYS_DOWN']:
+            if k in KEYS['DOWN']:
                 if not self.exam_paused and not self.is_exam_time_out:
                     self.selection_index += 1
 
-            elif k in KEYS['KEYS_UP']:
+            elif k in KEYS['UP']:
                 if not self.exam_paused and not self.is_exam_time_out:
                     self.selection_index -= 1
 
@@ -452,21 +495,21 @@ class Exam:
                     # Return the entered answer
                     return index, answer, correct
 
-            elif k in KEYS['KEYS_PAUSE']:
+            elif k in KEYS['PAUSE']:
                 self.exam_paused = True
 
-            elif k in KEYS['KEYS_RESUME']:
+            elif k in KEYS['RESUME']:
                 self.exam_paused = False
                 self.exam_quit = False
 
-            elif k in KEYS['KEYS_QUIT']:
+            elif k in KEYS['QUIT']:
                 if not self.is_exam_time_out:
                     self.exam_quit += 1
               
             ########################################################################################
 
             # Check terminal size
-            terminal_size_good = self.__check_terminal_size(scr, height_limit=30, width_limit=80)
+            terminal_size_good = self.__check_terminal_size(scr)
             if not terminal_size_good:
                 pass
 
