d931c32bf4f57e2bb9d430321ceb358ff37bf651
Ismet Handzic <ihandzic@xonar.com>
2020-10-23 17:39:36 -0400

    preflight testing

--- scripts/preflight.sh
+++ scripts/preflight.sh
@@ -9,7 +9,9 @@ CHANGED_FILES=`git diff --name-only master...${TRAVIS_COMMIT}`
 ONLY_READMES=True
 MD=".md"
 
+echo "Looking for all changed files ..."
 for CHANGED_FILE in $CHANGED_FILES; do
+  echo "Changed File: $CHANGED_FILE"
   if ! [[ $CHANGED_FILE =~ $MD ]]; then
     ONLY_READMES=False
     break
@@ -18,8 +20,10 @@ done
 
 if [[ $ONLY_READMES == True ]]; then
   echo "Only .md files have changed. Exiting ..."
-  travis_terminate 0
-  exit 0
+  # travis_terminate 0
+  exit 1
 else
   echo "Non-.md files have changed. Continuing with build ..."
 fi
+
+exit 0
