713d82da82efeb1612520fb2785937382e6c63a9
Ismet Handzic <ihandzic@xonar.com>
2020-10-23 17:27:45 -0400

    prefligh script for travis CI

---
+++ scripts/preflight.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Source:
+#   https://github.com/dev-id/Magic-Spoiler/blob/8111a06ab6682e020169991d5e2aa4fa503d787f/preflight.sh
+
+set -e
+
+CHANGED_FILES=`git diff --name-only master...${TRAVIS_COMMIT}`
+ONLY_READMES=True
+MD=".md"
+
+for CHANGED_FILE in $CHANGED_FILES; do
+  if ! [[ $CHANGED_FILE =~ $MD ]]; then
+    ONLY_READMES=False
+    break
+  fi
+done
+
+if [[ $ONLY_READMES == True ]]; then
+  echo "Only .md files found, exiting."
+  travis_terminate 0
+  exit 1
+else
+  echo "Non-.md files found, continuing with build."
+  python verify_files.py
+fi
