46089188e332eb3082a78432eb7dc9752b2e27f3
Ismet Handzic <ihandzic@xonar.com>
2020-10-23 18:16:28 -0400

    gave up on ignoring MD file changes :(

--- scripts/preflight.sh
+++ scripts/preflight.sh
@@ -2,24 +2,24 @@
 
 set -e
 
+# Parsing the script arguments
 CURRENT_BRANCH=$1
-echo "Current branch: $CURRENT_BRANCH"
-
-# TARGET_BRANCH='master'
 TARGET_BRANCH=$2
+echo "Current branch: $CURRENT_BRANCH"
 echo "Target branch: $TARGET_BRANCH"
 
 ONLY_SKIPPED_FILES=True
 MD=".md"
 echo "Files to ignore: ${MD}"
 
+# Checking the diff between branches
 echo "Looking for all changed files compared to target branch ..."
 CHANGED_FILES=$(git diff --name-only ${TARGET_BRANCH}...${CURRENT_BRANCH})
 
-echo "$CHANGED_FILES"
-
+# Looping through all changed filespaths
 for CHANGED_FILE in $CHANGED_FILES; do
   echo "Changed File: $CHANGED_FILE"
+  # Checking if at least one file is not an ingored file
   if ! [[ $CHANGED_FILE =~ $MD ]]; then
     ONLY_SKIPPED_FILES=False
     break
