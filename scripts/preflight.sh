abec2bcf5c8a9e4e6fd06c41d52518733dfcef30
Ismet Handzic <ihandzic@xonar.com>
2020-10-23 17:51:54 -0400

    more preflight

--- scripts/preflight.sh
+++ scripts/preflight.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-# Source:
-#   https://github.com/dev-id/Magic-Spoiler/blob/8111a06ab6682e020169991d5e2aa4fa503d787f/preflight.sh
-
 set -e
 
-CHANGED_FILES=`git diff --name-only master...${TRAVIS_COMMIT}`
-ONLY_READMES=True
-MD=".md"
+CURRENT_BRANCH='git rev-parse --abbrev-ref HEAD'
+echo "Current branch: $CURRENT_BRANCH"
 
-echo "Looking for all changed files ..."
+TARGET_BRANCH='master'
+echo "Target branch: $TARGET_BRANCH"
+
+ONLY_SKIPPED_FILES=True
+MD=".md"
+echo "Files to ignore: ${MD}"
+
+echo "Looking for all changed files compared to target branch ..."
+CHANGED_FILES=`git diff --name-only ${TARGET_BRANCH}...${CURRENT_BRANCH}`
+
 for CHANGED_FILE in $CHANGED_FILES; do
   echo "Changed File: $CHANGED_FILE"
   if ! [[ $CHANGED_FILE =~ $MD ]]; then
-    ONLY_READMES=False
+    ONLY_SKIPPED_FILES=False
     break
   fi
 done
 
-if [[ $ONLY_READMES == True ]]; then
-  echo "Only .md files have changed. Exiting ..."
-  # travis_terminate 0
+if [[ $ONLY_SKIPPED_FILES == True ]]; then
+  echo "Only $MD files have changed. Exiting ..."
   exit 1
 else
-  echo "Non-.md files have changed. Continuing with build ..."
+  echo "No $MD files have changed. Continuing with build ..."
 fi
 
 exit 0
