81d1d45e160dc950f20ebc8a258b4b5a0276127a
Ismet Handzic <ismet.handzic@gmail.com>
2020-11-16 09:13:19 -0500

    Bump version: 0.2.3 → 0.2.4

--- exam_terminal/utility.py
+++ exam_terminal/utility.py
@@ -117,7 +117,7 @@ def load_software_name_version() -> str:
         (str): Software name and version
     """
     software_name = "exam-terminal"
-    software_version = "0.2.3"  # Updated with bumpversion
+    software_version = "0.2.4"  # Updated with bumpversion
     return software_name + ' v' + software_version
 
 
