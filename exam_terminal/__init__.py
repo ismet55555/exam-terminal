1bed378a1e1e2fa16f233b3a1d9da5251e09f49e
Ismet Handzic <ismet55555@users.noreply.github.com>
2023-12-25 00:40:00 -0500

    [bug] Fix dependencies (requests) (#75)
    * Fix dependencies
    
    * Bump version: 0.2.10 → 0.2.11

--- exam_terminal/__init__.py
+++ exam_terminal/__init__.py
@@ -2,6 +2,6 @@
 
 __title__ = 'exam-terminal'
 __summary__ = "A terminal-based exam, text, or survey tool for educators and learners"
-__version__ = "0.2.10"
+__version__ = "0.2.11"
 __author__ = "Ismet Handzic"
 __copyright__ = "Copyright 2024 %s" % __author__
