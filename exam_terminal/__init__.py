02d581fdf3fe0c6809f8e9a2756780c51049d938
Ismet Handzic <ismet.handzic@gmail.com>
2023-12-24 22:28:33 -0500

    Various imporvements

--- exam_terminal/__init__.py
+++ exam_terminal/__init__.py
@@ -1,2 +1,7 @@
-# This file blank
-# Used to indicate to python that this directory is a package
+"""exam-terminal package version definition."""
+
+__title__ = 'exam-terminal'
+__summary__ = "A terminal-based exam, text, or survey tool for educators and learners"
+__version__ = "0.2.9"
+__author__ = "Ismet Handzic"
+__copyright__ = "Copyright 2024 %s" % __author__
