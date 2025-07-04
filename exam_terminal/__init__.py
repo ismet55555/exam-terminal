ba6a2b33a5f3ba12dee34736fbbe3338f50d4829
Ismet Handzic <ismet.handzic@gmail.com>
2025-07-04 15:29:53 -0400

    Bump version: 0.2.12 → 0.2.13

--- exam_terminal/__init__.py
+++ exam_terminal/__init__.py
@@ -2,6 +2,6 @@
 
 __title__ = 'exam-terminal'
 __summary__ = 'A terminal-based exam, text, or survey tool for educators and learners'
-__version__ = '0.2.12'
+__version__ = '0.2.13'
 __author__ = 'Ismet Handzic'
 __copyright__ = f'Copyright 2024 {__author__}'
